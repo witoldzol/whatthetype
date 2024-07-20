2ce1ebf5d8e33519b7f73fc09780ccaa29f1473c
Witold Zolnowski <blue3656@gmail.com>
2024-07-20 19:55:05 +0100

    refactor

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pudb
 import sys
 from enum import Enum
 
+FUNC_VARIABLES = {}
+PROJECT_NAME="typemedaddy"
+
 class TraceEvent(Enum):
     CALL='call' #: Triggered when a function is called.
     LINE='line' #: Triggered when a new line of code is executed.
     RETURN='return' #: Triggered when a function is about to return.
     EXCEPTION='exception' #: Triggered when an exception is raised.
-
     # if string, compare to value by default
     def __eq__(self, other):
         if isinstance(other, str):
@@ -16,11 +18,22 @@ class TraceEvent(Enum):
         return super().__eq__(other)
 
 def trace_function(frame, event, arg):
-    # print(f"{frame=}")
     if event == TraceEvent.CALL:
-        print(f"{event=}")
-        print("Function name:", frame.f_code.co_name)
-        print("Local variables:", frame.f_locals)
+        module_name =frame.f_code.co_filename
+        func_name = frame.f_code.co_name
+        if PROJECT_NAME not in module_name or func_name == "trace":
+            return trace_function
+        local_vars = frame.f_locals
+        if module_name not in FUNC_VARIABLES:
+            FUNC_VARIABLES[module_name] = {}
+        if func_name not in FUNC_VARIABLES[module_name]:
+            FUNC_VARIABLES[module_name][func_name] = {}
+        for k, v in local_vars.items():
+            FUNC_VARIABLES[module_name][func_name][k] = v
+        # print("Function name:", func_name)
+        # print(f"Function module: {module}")
+        # print("Local variables:", local_vars)
+        
     # print("Global variables:", list(frame.f_globals.keys()))
     return trace_function
 
@@ -38,4 +51,5 @@ def trace():
 
 with trace():
     result = example_function(1, 2)
-    print(result)
+    print(FUNC_VARIABLES)
+
