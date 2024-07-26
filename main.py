c3ef4d0cdd49b91c1d1bb72266dc984af7fb3121
witold zolnowski <blue3656@gmail.com>
2024-07-26 08:28:25 +0100

    temp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import contextlib
 import sys
 from enum import Enum
 from foo import example_function
+from foo import Foo
 
 FUNC_VARIABLES = {}
 MODEL = {
@@ -27,8 +28,52 @@ class TraceEvent(Enum):
             return self.value == other
         return super().__eq__(other)
 
+def is_user_defined_class(obj):
+    return isinstance(obj, object) and not isinstance(obj, (int, float, str, list, dict, tuple, set))
 
 def trace_function(frame, event, arg):
+    module_name = frame.f_code.co_filename
+    func_name = frame.f_code.co_name
+    line_number = example_function.__code__.co_firstlineno
+    mod_func_line = f"{module_name}:{func_name}:{line_number}"
+    function_arg_count = frame.f_code.co_argcount
+    arg_names = frame.f_code.co_varnames[:function_arg_count]
+    local_vars = frame.f_locals
+    if PROJECT_NAME not in module_name or func_name == "trace":
+        return trace_function
+    if mod_func_line not in FUNC_VARIABLES:
+        FUNC_VARIABLES[mod_func_line] = {"args": {}}
+    print(f"ARG NAMES: {arg_names}")
+    if event == TraceEvent.CALL:
+        for name in arg_names:
+            # ignore self references
+            if name == "self":
+                continue
+            var = local_vars[name]
+            var_type = type(var).__name__
+            # don't return type, just value, ( unless it's a class - then capture a name )
+            if is_user_defined_class(var):
+                print("USER CLASS ======================")
+                print("VAR TYPE === ", var_type)
+                print(var)
+            else:
+                print("not a class ", var_type)
+
+            if name in FUNC_VARIABLES[mod_func_line]["args"]:
+                FUNC_VARIABLES[mod_func_line]["args"][name].add(var_type)
+            else:
+                FUNC_VARIABLES[mod_func_line]["args"][name] = set([var_type])
+    elif event == TraceEvent.RETURN:
+        print(f"RETURN : {arg}")
+        return_type = type(arg).__name__
+        if "return" in FUNC_VARIABLES[mod_func_line]:
+            FUNC_VARIABLES[mod_func_line]["return"].add(return_type)
+        else:
+            FUNC_VARIABLES[mod_func_line]["return"] = set([return_type])
+    print(f"FUNC_VARIABLES :\n {FUNC_VARIABLES}")
+    return trace_function
+
+def trace_function_bak(frame, event, arg):
     module_name = frame.f_code.co_filename
     func_name = frame.f_code.co_name
     line_number = example_function.__code__.co_firstlineno
@@ -77,7 +122,8 @@ def trace():
 
 def run():
     with trace():
-        result = example_function(1, 2)
+        f = Foo("bar")
+        result = example_function(1, 2, f)
         print(FUNC_VARIABLES)
 
 
