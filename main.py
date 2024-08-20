e9cfdba34a6b68ea9f338e3f0a16b9716cd55ad0
Witold Zolnowski <blue3656@gmail.com>
2024-08-20 22:01:14 +0100

    rename func_variables to result, to be more explicit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from foo import example_function, function_taking_nested_class
 from foo import Foo
 from nested.inner.bar import Bar
 
-FUNC_VARIABLES = {}
+RESULT = {}
 MODEL = {
     "module:func_name:func_line": {
         "args": {
@@ -43,8 +43,8 @@ def trace_function(frame, event, arg):
     if PROJECT_NAME not in module_name or func_name == "trace":
         return trace_function
     # setup dictionary
-    if mod_func_line not in FUNC_VARIABLES:
-        FUNC_VARIABLES[mod_func_line] = {"args": {}}
+    if mod_func_line not in RESULT:
+        RESULT[mod_func_line] = {"args": {}}
     ##### CALL #####
     if event == TraceEvent.CALL:
         for name in function_arg_names:
@@ -66,68 +66,32 @@ def trace_function(frame, event, arg):
             else:
                 print("not a class ", var_type)
 
-            if name in FUNC_VARIABLES[mod_func_line]["args"]:
-                FUNC_VARIABLES[mod_func_line]["args"][name].add(var_type)
+            if name in RESULT[mod_func_line]["args"]:
+                RESULT[mod_func_line]["args"][name].add(var_type)
             else:
-                FUNC_VARIABLES[mod_func_line]["args"][name] = set([var_type])
+                RESULT[mod_func_line]["args"][name] = set([var_type])
     ##### RETURN #####
     elif event == TraceEvent.RETURN:
         print(f"RETURN : {arg}")
         return_type = type(arg).__name__
-        if "return" in FUNC_VARIABLES[mod_func_line]:
-            FUNC_VARIABLES[mod_func_line]["return"].add(return_type)
+        if "return" in RESULT[mod_func_line]:
+            RESULT[mod_func_line]["return"].add(return_type)
         else:
-            FUNC_VARIABLES[mod_func_line]["return"] = set([return_type])
-    print(f"FUNC_VARIABLES :\n {FUNC_VARIABLES}")
+            RESULT[mod_func_line]["return"] = set([return_type])
+    print(f"FUNC_VARIABLES :\n {RESULT}")
     return trace_function
-
-def trace_function_bak(frame, event, arg):
-    module_name = frame.f_code.co_filename
-    func_name = frame.f_code.co_name
-    line_number = example_function.__code__.co_firstlineno
-    mod_func_line = f"{module_name}:{func_name}:{line_number}"
-    function_arg_count = frame.f_code.co_argcount
-    arg_names = frame.f_code.co_varnames[:function_arg_count]
-    local_vars = frame.f_locals
-    if PROJECT_NAME not in module_name or func_name == "trace":
-        return trace_function
-    if mod_func_line not in FUNC_VARIABLES:
-        FUNC_VARIABLES[mod_func_line] = {"args": {}}
-    # FUNCTION CALL with args
-    if event == TraceEvent.CALL:
-        # name refers to parameter name defined in function signature
-        for name in arg_names:
-            if name == "self":
-                continue
-            var = local_vars[name]
-            var_type = type(var).__name__
-            # if we find this argument for that function, just add more data ( we will use / dedup later )
-            if name in FUNC_VARIABLES[mod_func_line]["args"]:
-                FUNC_VARIABLES[mod_func_line]["args"][name].add(var_type)
-            else:
-                # otherwise, if we see this arg for the first time, create a new entry
-                FUNC_VARIABLES[mod_func_line]["args"][name] = set([var_type])
-    # FUNCTION RETURN
-    elif event == TraceEvent.RETURN:
-        return_type = type(arg).__name__
-        if "return" in FUNC_VARIABLES[mod_func_line]:
-            FUNC_VARIABLES[mod_func_line]["return"].add(return_type)
-        else:
-            FUNC_VARIABLES[mod_func_line]["return"] = set([return_type])
-    return trace_function
-
 
 @contextlib.contextmanager
 def trace():
-    global FUNC_VARIABLES
+    global RESULT
     print("========== TRACING ON ==========")
     sys.settrace(trace_function)
     try:
-        yield FUNC_VARIABLES
+        yield RESULT
     finally:
         print("========== TRACING OFF ==========")
         sys.settrace(None)
-        FUNC_VARIABLES = {}
+        RESULT = {}
 
 
 if __name__ == "__main__":
@@ -137,4 +101,4 @@ if __name__ == "__main__":
         lol = Bar()
 
         bar_name = function_taking_nested_class(lol)
-        print(FUNC_VARIABLES)
+        print(RESULT)
