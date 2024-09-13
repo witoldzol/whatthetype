743e522ad759174856fd98ecd3aea86e58f03183
Witold Zolnowski <blue3656@gmail.com>
2024-09-13 07:36:08 +0100

    fix second test

--- test_main.py
+++ test_main.py
@@ -26,17 +26,20 @@ def test_example_function():
             assert actual[k]["return"] == [3]
 
 
-# def test_if_global_context_is_not_polluted_by_previous_test_invocation():
-#     with trace() as actual:
-#         f = Foo()
-#         example_function(1, 2, f)
-#     for k in actual:
-#         if 'init' in k:
-#             assert actual[k]["args"] == {"bar": {"NoneType"}}
-#         elif 'get_foo' in k:
-#             assert actual[k]["args"] == {"a": {"int"}, "b": {"int"}, "foo": {"Foo"}}
-#             assert actual[k]["return"] == {"int"}
-#
+def test_if_global_context_is_not_polluted_by_previous_test_invocation():
+    with trace() as actual:
+        f = Foo()
+        example_function(1, 2, f)
+        example_function(3, 4, None)
+    for k in actual:
+        if 'init' in k:
+            assert actual[k]["args"] == {"bar": [None]}
+            assert actual[k]["return"] == [None]
+        elif 'example_function' in k:
+            assert actual[k]["args"] == {"a": [1, 3], "b": [2, 4], "foo": ["USER_CLASS|foo::Foo", None]}
+            assert actual[k]["return"] == [3, 7]
+
+
 # def test_example_function_with_different_args():
 #     with trace() as actual:
 #         f = Foo()
