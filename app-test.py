b1947405753e12a869666bba625aa08eb6b7dd00
Witold Zolnowski <blue3656@gmail.com>
2024-09-21 07:56:01 +0100

    hardcode input in test app

--- app-test.py
+++ app-test.py
@@ -2,18 +2,13 @@ import main
 
 def print_stuff(input):
     print(input)
+    print(type(input))
 
-def interactive_input():
-    print("Enter your inputs (type 'done' when finished):")
-    inputs = []
-    while True:
-        user_input = input("> ")
-        if user_input.lower() == "done":
-            break
-        inputs.append(user_input)
+def fake_interactive_input():
+    inputs = [1,1.0,"bob"]
     print("\nYou entered:")
     for item in inputs:
         print_stuff(item)
 
 if __name__ == "__main__":
-    interactive_input()
+    fake_interactive_input()
