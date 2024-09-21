dab01afcfd84ee4e74aa9b58f8a8f2cb1eb0d089
Witold Zolnowski <blue3656@gmail.com>
2024-09-21 07:37:06 +0100

    make test app interactive

--- app-test.py
+++ app-test.py
@@ -1,10 +1,19 @@
-import argparse
+import main
 
 def print_stuff(input):
     print(input)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input", default=1111)
-    args = parser.parse_args()
-    print_stuff(args)
+def interactive_input():
+    print("Enter your inputs (type 'done' when finished):")
+    inputs = []
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == "done":
+            break
+        inputs.append(user_input)
+    print("\nYou entered:")
+    for item in inputs:
+        print_stuff(item)
+
+if __name__ == "__main__":
+    interactive_input()
