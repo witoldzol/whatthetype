2ef7daa6f578942baed2cf93d983126552b97a59
Witold Zolnowski <blue3656@gmail.com>
2024-09-21 07:57:09 +0100

    trace test app

--- app-test.py
+++ app-test.py
@@ -2,7 +2,6 @@ import main
 
 def print_stuff(input):
     print(input)
-    print(type(input))
 
 def fake_interactive_input():
     inputs = [1,1.0,"bob"]
@@ -11,4 +10,5 @@ def fake_interactive_input():
         print_stuff(item)
 
 if __name__ == "__main__":
-    fake_interactive_input()
+    with main.trace():
+        fake_interactive_input()
