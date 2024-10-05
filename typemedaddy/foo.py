b4ab30fd47a4c43d5a7956d6c885f18ad5d5b0e8
Witold Zolnowski <blue3656@gmail.com>
2024-10-05 07:07:58 +0100

    add default value to test function

--- typemedaddy/foo.py
+++ typemedaddy/foo.py
@@ -9,7 +9,7 @@ class Foo:
     def get_foo(self, name, age):
         return f"{name},{age}"
 
-    def arbitrary_self(not_self, name, age):
+    def arbitrary_self(not_self, name, age=10):
         not_self.bar = 'lol'
         return f"{name},{age}"
 
