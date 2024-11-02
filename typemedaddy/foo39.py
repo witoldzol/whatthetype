8f100501cb658e7889efcf4a2c5d15078a400693
Witold Zolnowski <blue3656@gmail.com>
2024-11-02 07:24:16 +0000

    add test funcs for python 3.9

---
+++ typemedaddy/foo39.py
@@ -0,0 +1,20 @@
+from typemedaddy.nested.inner.bar import Bar
+
+class Foo:
+    def __init__(self, bar: Bar=None):
+        self.bar = bar
+
+    def get_foo(self, name, age):
+        return f"{name},{age}"
+
+    def arbitrary_self(not_self, name: str = 'default_val', age=10):
+        not_self.bar = 'lol'
+        return f"{name},{age}"
+    
+    def args_kwargs_func(self, *args, **kwargs):
+        return 'lol'
+
+def example_function(a, b, foo) -> int:
+    if type(a) is str and type(b) is int:
+        a = int(a)
+    return a + b
