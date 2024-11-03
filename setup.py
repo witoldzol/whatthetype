7ce702ee452867fbd3ec02138e09db5c95e7efe4
Witold Zolnowski <blue3656@gmail.com>
2024-11-03 13:41:19 +0000

    update setup, require autopep8

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="typemedaddy",
-    version="0.1",
+    version="0.2",
     packages=["typemedaddy"],
     author="Witold Zolnowski",
     description="Derives type hints from data captured during runtime. Updates source files with type hints. USE SOURCE CONTROL before using!!!!",
+    install_requires=["autopep8>=2.0.0"],
 )
