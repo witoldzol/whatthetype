b2b4b3ede41b44c02606122b65ed8a5887235a30
Witold Zolnowski <blue3656@gmail.com>
2024-11-03 09:16:28 +0000

    add description to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name="typemedaddy",
-    version="0.1",
-    packages=['typemedaddy'],
-    author='Witold Zolnowski'
+    version="0.2",
+    packages=["typemedaddy"],
+    author="Witold Zolnowski",
+    description="Derives type hints from data captured during runtime. Updates source files with type hints. USE SOURCE CONTROL before using!!!!",
 )
