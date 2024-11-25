2a556cafb68b09ecc04a3bac9d2f5a982a654575
Witold Zolnowski <blue3656@gmail.com>
2024-11-25 10:21:04 +0000

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (current_directory / "README.md").read_text()
 
 setup(
     name="typemedaddy",
-    version="0.3",
+    version="0.4",
     packages=["typemedaddy"],
     author="Witold Zolnowski",
     description="Derives type hints from data captured during runtime. Updates source files with type hints. USE SOURCE CONTROL before running!!!!",
