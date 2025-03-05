4fa0683ce4d18fe8ab18b44203146ebddaf8e3ab
Witold Zolnowski <blue3656@gmail.com>
2025-03-05 07:56:22 +0000

    bump version 1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (current_directory / "README.md").read_text()
 
 setup(
     name="whatthetype",
-    version="0.9",
+    version="1.0",
     packages=["whatthetype"],
     author="Witold Zolnowski",
     description="Derives type hints from data captured during runtime. Updates source files with type hints. USE SOURCE CONTROL before upating in-place!!!!",
